2b431e4019ee0eab8cb8110dc2edbd8ea7487185
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-04-25 01:00:58 -0500

    17. Testing the console.table() to show data in the 3rd exercise

--- weekly_mission_2/classes_and_objects/classes3.js
+++ weekly_mission_2/classes_and_objects/classes3.js
@@ -11,4 +11,4 @@ class Student {
 // Crear un objeto de la clase Student (esto se le llama instanciación)
 const carloStudent = new Student("Carlo", 12, ["NodeJs", "Python"]);
 console.log("Ejemplo 3: Instanciar un objeto con atributos");
-console.log(carloStudent);
+console.table(carloStudent);
