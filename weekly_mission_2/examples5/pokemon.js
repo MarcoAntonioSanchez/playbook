227e6ea08d19a3f7ceffffede4f36c9fb7bb63b2
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-04-28 06:29:13 -0500

    26. Npm install and solving 4th exercise on the new pokemon class with npm test

--- weekly_mission_2/examples5/pokemon.js
+++ weekly_mission_2/examples5/pokemon.js
@@ -1,22 +1,23 @@
 /*
+pokemon.js es la definición de una clase
 Este ejemplo incluye dependencias. En tu línea de comando ve a este proyecto y ejecuta el comando `npm install` para descargar las dependencias, esto creará una carpeta node_modules
 */
 export default class Pokemon {
-  constructor(name, type, age){
-    this.name = name
-    this.type = type
-    this.age = age
-    this.attacks = []
+  constructor(name, type, age) {
+    this.name = name;
+    this.type = type;
+    this.age = age;
+    this.attacks = [];
   }
 
-  get getAttacks(){
-    return this.attacks
+  get getAttacks() {
+    return this.attacks;
   }
 
-  set setAttacks(attacks){
-    return this.attacks = attacks
+  set setAttacks(attacks) {
+    return (this.attacks = attacks);
   }
 }
 
-const myPokemon = new Pokemon("Charmander", "Fuego", 10)
-console.log(myPokemon)
+const myPokemon = new Pokemon("Charmander", "Fuego", 10);
+console.log(myPokemon);
