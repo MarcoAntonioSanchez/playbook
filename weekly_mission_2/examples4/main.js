b0073c56d3e1b315d14091bece5786e02d27dbcd
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-04-28 06:31:26 -0500

    27. Adding the examples4 exercises

--- weekly_mission_2/examples4/main.js
+++ weekly_mission_2/examples4/main.js
@@ -6,7 +6,7 @@ import Viajero from "./viajero.js";
 Este es un ejemplo de como modularizar clases y usarlas mediante módulos.
 */
 
-const viajero1 = new Viajero("Carlo", "LaunchX", "Node JS", "Abril 2022");
+const viajero1 = new Viajero("Marco", "LaunchX", "Node JS", "Abril 2022");
 console.log(viajero1);
 console.log(viajero1.getNameAndUsername()); // Método de la clase padre
 console.log(viajero1.getGeneralInfo()); // Método de la clase hija
