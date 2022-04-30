68b195fa090ce1823192b87feb6d8acd43d59e6d
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-04-30 15:15:31 -0500

    34. Solving the 1st exercise on weekly_mission_2

--- weekly_mission_2/exercises/exercise1/repo.js
+++ weekly_mission_2/exercises/exercise1/repo.js
@@ -18,3 +18,40 @@ const repo = {
 console.log("Nombre del repo:" + repo.name);
 console.log("Issues totales: " + repo.getTotalIssues());
 console.log(repo.getGeneralInfo());
+
+const issue = {
+  title: "duda número 1",
+  repositoryNameAssociated: "nombre del repo",
+  status: "status",
+  numberOfCommits: 33,
+  labels: "launchx",
+  author: "MarcoAntonioSanchez",
+  dateCreated: "April 21",
+  lastUpdated: "April 22",
+  getTitleAndAuthor: function () {
+    return this.title + " por: " + this.author;
+  },
+  getGeneralInfo: function () {
+    return `This issue was created on: ${this.dateCreated} and last updated on: ${this.lastUpdated}`;
+  },
+};
+
+console.log(issue.getTitleAndAuthor());
+console.log(issue.getGeneralInfo());
+
+const pullRequest = {
+  title: "PR número 1",
+  branchName: "nombre del branch",
+  dateCreated: "April 23",
+  status: "estado",
+  repositoryNameAssociated: "Marco",
+  getStatus: function () {
+    return this.status;
+  },
+  getTitleAndAuthor: function () {
+    return `This pull request ${this.title} was created by ${this.repositoryNameAssociated}`;
+  },
+};
+
+console.log(pullRequest.getStatus());
+console.log(pullRequest.getTitleAndAuthor());
