65cb74cb5bbceb3fb71e4d5046341b48bdd07d1e
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-05-21 22:07:49 -0500

    73. Replacing the 1st test for the true test on the instanced class for Mission Commander object

--- weekly_mission_3/exercise_0/my_launchx_app/test/missionCommander.test.js
+++ weekly_mission_3/exercise_0/my_launchx_app/test/missionCommander.test.js
@@ -1,6 +1,8 @@
-describe("Esto es una suite de pruebas", () => {
-  test("Caso de prueba 1", () => {
-    const result = 1 + 2;
-    expect(result).toBe(3);
+const MissionCommander = require("./../app/missionCommander");
+
+describe("Unit Tests for Mission Commande Class", () => {
+  test("1) Creatre a mission commander object", () => {
+    const myMissionCommander = new MissionCommander("Woopa");
+    expect(myMissionCommander.name).toBe("Woopa");
   });
 });
