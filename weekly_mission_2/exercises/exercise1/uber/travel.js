a2c9db862b866cbf3c2dc48c8f1aae751a1c77be
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-05-21 21:08:32 -0500

    64. Fixing the 2nd uber object model

--- weekly_mission_2/exercises/exercise1/uber/travel.js
+++ weekly_mission_2/exercises/exercise1/uber/travel.js
@@ -5,22 +5,6 @@ const travel = {
   ellapsedTime: "05:33 mints.",
   driver: "José Luis",
   passenger: "Rigoberto",
-  getDestinationEstTime: function () {
-    return this.finalDestination + this.estimatedTime;
-  },
-  getDriversName: function () {
-    return this.driver;
-  },
-  getPassengersName: function () {
-    return this.passenger;
-  },
 };
 
-console.log(
-  "Tiempo estimado y destino final: " +
-    travel.estimatedTime +
-    " " +
-    travel.finalDestination
-);
-console.log("Conductor: " + travel.getDriversName());
-console.log("Pasajero: " + travel.getPassengersName());
+console.log(travel);
