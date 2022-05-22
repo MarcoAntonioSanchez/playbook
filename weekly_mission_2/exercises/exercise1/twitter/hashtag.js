2582da8a7c98b61b222a17c5d01a5214febfe7d9
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-05-21 20:50:34 -0500

    60. Fixing the 1st twitter object model

--- weekly_mission_2/exercises/exercise1/twitter/hashtag.js
+++ weekly_mission_2/exercises/exercise1/twitter/hashtag.js
@@ -5,14 +5,6 @@ const hashtag = {
   comments: 50,
   likes: 200,
   retweets: 1500,
-  getTotalInteractions: function () {
-    return this.comments + this.likes + this.retweets;
-  },
-  getGeneralInfo: function () {
-    return `This hashtag ${this.content} was published on ${this.publishDate} by: ${this.author}`;
-  },
 };
 
-console.log("Nombre del hashtag: " + hashtag.content);
-console.log("Interacciones totales: " + hashtag.getTotalInteractions());
-console.log(hashtag.getGeneralInfo());
+console.log(hashtag);
