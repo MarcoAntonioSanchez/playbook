13390ad08fc889542121d0006adbf958ae7942d4
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-05-21 22:34:17 -0500

    79. Adding the start line on the scripts section into the package.json file

--- weekly_mission_3/exercise_1/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/exercise_1/spiderverse/test/spiderman.test.js
@@ -1,6 +1,13 @@
-describe("Test Suite DUmmy Description", () => {
-  test("Case 1 Dummy", () => {
-    const resultOfSomething = 1 + 2;
+describe("Unit Tests for Spiderman class", () => {
+  test("1) Create an spiderman object", () => {
+    // Aquí escribimos el código que queremos usar tal cuiál
+    // Quiero poder instanciar un objecto Spiderman con esta información
+    const andrewGarfield = new Spiderman(
+      "Spiderman Sony",
+      31,
+      "Andrew Garfield",
+      2
+    );
     expect(resultOfSomething).toBe(18);
   });
 });
