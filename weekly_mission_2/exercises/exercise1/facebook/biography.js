753932b29deaa297b379ec3cfda2593699fe2e51
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-05-21 20:44:22 -0500

    56. Fixing the 1st facebook object model

--- weekly_mission_2/exercises/exercise1/facebook/biography.js
+++ weekly_mission_2/exercises/exercise1/facebook/biography.js
@@ -9,28 +9,4 @@ const bio = {
   pictures: ["profile pictures", "portrait", "uploads"],
 };
 
-class newBio {
-  constructor(profile, about, birthday, relationStatus, friends, followers) {
-    this.profile = profile;
-    this.about = about;
-    this.birthday = birthday;
-    this.relationStatus = relationStatus;
-    this.friends = friends;
-    this.followers = followers;
-  }
-
-  checkBio() {
-    return `${this.profile} is a brand new bio profile`;
-  }
-}
-
-const facebookBio = new newBio(
-  "@NewProfile",
-  "This is a new profile",
-  "01 01 01",
-  true,
-  01,
-  01
-);
-console.log(facebookBio);
-console.log(facebookBio.checkBio());
+console.log(bio);
