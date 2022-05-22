7f50694c5d91277a251995aa7d08c84b9ee65f05
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-05-21 20:51:28 -0500

    61. Fixing the 2nd twitter object model

--- weekly_mission_2/exercises/exercise1/twitter/trendingTopic.js
+++ weekly_mission_2/exercises/exercise1/twitter/trendingTopic.js
@@ -4,14 +4,6 @@ const trendingTopic = {
   comment: "Este 30 de abril se celebra el día del niño en México",
   retweet: false,
   totalTweets: 35800,
-  getTotalTweets: function () {
-    return this.totalTweets;
-  },
-  getGeneralInfo: function () {
-    return `This trending topic ${this.title} has this hashtag ${this.hashtag}`;
-  },
 };
 
-console.log("Titulo del trending:" + trendingTopic.title);
-console.log("Tweets totales: " + trendingTopic.getTotalTweets());
-console.log(trendingTopic.getGeneralInfo());
+console.log(trendingTopic);
