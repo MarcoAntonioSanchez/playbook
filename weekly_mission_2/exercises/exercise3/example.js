61b505f930e0b461a92bcd5cff5b91a6920c4974
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-05-21 19:38:42 -0500

    51. Adding the example.js file on the 3rd exercise of weekly mission 2

---
+++ weekly_mission_2/exercises/exercise3/example.js
@@ -0,0 +1,14 @@
+console.log("POO");
+
+// objeto de js
+const woopa = {
+  name: "Woopa",
+  mission: "Node JS",
+  age: "27",
+  color: "pink",
+};
+
+// crear una clase
+class Ajolonauta {}
+
+console.log(Ajolonauta);
