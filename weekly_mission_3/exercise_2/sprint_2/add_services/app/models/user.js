a3613e2029f165653d57c1d16968039af8d5c69a
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-05-22 20:04:31 -0500

    99. Solving the test on the requirement # 1

---
+++ weekly_mission_3/exercise_2/sprint_2/add_services/app/models/user.js
@@ -0,0 +1,10 @@
+class User {
+  constructor(username, name, id, bio) {
+    this.username = username;
+    this.name = name;
+    this.id = id;
+    this.bio = bio;
+  }
+}
+
+module.exports = User;
--- weekly_mission_3/exercise_2/sprint_2/add_services/test/services/UserService.test.js
+++ weekly_mission_3/exercise_2/sprint_2/add_services/test/services/UserService.test.js
@@ -2,7 +2,7 @@ const UserService = require("../../app/services/UserService");
 
 describe("Tests for UserService", () => {
   test("1. Create a new user using the UserService", () => {
-    const user = UserService.create(1, "carlogilmar", "Carlo");
+    const user = UserService.create("carlogilmar", "Carlo", 1);
     expect(user.username).toBe("carlogilmar");
     expect(user.name).toBe("Carlo");
     expect(user.id).toBe(1);
