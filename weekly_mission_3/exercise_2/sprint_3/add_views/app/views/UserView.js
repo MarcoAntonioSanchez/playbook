dbeb91003fe424918bb497525ad6e42fb880ee64
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-05-25 22:24:50 -0500

    117. Adding the UserView class to return a payload message

--- weekly_mission_3/exercise_2/sprint_3/add_views/app/views/UserView.js
+++ weekly_mission_3/exercise_2/sprint_3/add_views/app/views/UserView.js
@@ -1,1 +1,9 @@
 const User = require("../../app/models/User");
+
+class UserView {
+  static createUser(id, username, name) {
+    return new User(id, username, name);
+  }
+}
+
+module.exports = UserView;
