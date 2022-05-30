db146e58ebdd8b23b00c15a1a583a7b604aaf899
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-05-29 20:06:43 -0500

    03. Creating the app.js file for the express app

---
+++ weekly_mission_3/exercise_3/express_basic/app.js
@@ -0,0 +1,18 @@
+// Using express object
+const express = require("express");
+
+// Express app
+const app = express();
+
+// Port where we are gonna get to our app: Localhost:3000
+const port = 3000;
+
+// Initial path, will response to url: Localhost:3000
+app.get("/", (req, res) => {
+  res.send("Hello friend");
+});
+
+// Initializing the app
+app.listen(port, () => {
+  console.log(`Example app listening on port: ${port}`);
+});
