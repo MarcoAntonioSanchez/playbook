af988446773f7cead7fe8557da7c76118aab6540
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-05-29 21:53:37 -0500

    06. Adding the 2nd method GET, but with an error

--- weekly_mission_3/exercise_3/express_api/app.js
+++ weekly_mission_3/exercise_3/express_api/app.js
@@ -22,4 +22,12 @@ app.get("/v1/explorers", (req, res) => {
   const explorer4 = { id: 4, name: "Marco" };
   const explorers = [explorer1, explorer2, explorer3, explorer4];
   res.status(200).json(explorers);
+
+  // Revisit for error
+  app.get("/v1/explorers/:id", (req, res) => {
+    console.log(`Api Explorers GET request ${new Date()}`);
+    console.log(`Getting explorer with id ${req.params.id}`);
+    const explorer = { id: 1, name: "Marco" };
+    res.status(200).json(explorer);
+  });
 });
