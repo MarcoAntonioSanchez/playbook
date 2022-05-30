8ca98ccaac85788cdba40d42b8d29cbccb2712e2
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-05-29 20:15:07 -0500

    07. Adding a 4rth route with query params

--- weekly_mission_3/exercise_3/express_basic/app.js
+++ weekly_mission_3/exercise_3/express_basic/app.js
@@ -25,6 +25,13 @@ app.get("/explorersInNode", (req, res) => {
   res.send(explorer);
 });
 
+// Query params: Get params from the url
+// http://localhost:3000/explorers/carlo
+// req.params = {"explorerName":"carlo"}
+app.get("/explorers/:explorerName", (req, res) => {
+  res.send(req.params);
+});
+
 // Initializing the app
 app.listen(port, () => {
   console.log(`Example app listening on port: ${port}`);
