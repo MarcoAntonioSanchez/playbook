86a2278161a0589f0b022c7dff27ec96472a0b00
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-05-29 21:13:04 -0500

    04. Adding the GET method with some objects to get a 200 http code for the status

--- weekly_mission_3/exercise_3/express_api/app.js
+++ weekly_mission_3/exercise_3/express_api/app.js
@@ -12,3 +12,14 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+// HTTP Methods
+app.get("/v1/explorers", (req, res) => {
+  console.log(`APi Explorers GET ALL requests ${new Date()}`);
+  const explorer1 = { id: 1, name: "Marco1" };
+  const explorer2 = { id: 1, name: "Marco2" };
+  const explorer3 = { id: 1, name: "Marco3" };
+  const explorer4 = { id: 1, name: "Marco" };
+  const explorers = [explorer1, explorer2, explorer3, explorer4];
+  res.status(200).json(explorers);
+});
