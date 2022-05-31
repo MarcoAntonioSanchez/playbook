8b886379beb4349b24a741068e4a3b7bc67497f3
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2022-05-31 00:19:24 -0500

    07. Adding the PUT and DELETE methods

--- weekly_mission_3/exercise_3/express_api/app.js
+++ weekly_mission_3/exercise_3/express_api/app.js
@@ -1,3 +1,5 @@
+// My 1st API
+
 // Using Express object
 const express = require("express");
 
@@ -13,6 +15,7 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
+// Create an endpoint that returns a list of explorers
 // HTTP Methods
 app.get("/v1/explorers", (req, res) => {
   console.log(`APi Explorers GET ALL requests ${new Date()}`);
@@ -22,12 +25,35 @@ app.get("/v1/explorers", (req, res) => {
   const explorer4 = { id: 4, name: "Marco" };
   const explorers = [explorer1, explorer2, explorer3, explorer4];
   res.status(200).json(explorers);
+});
 
-  // Revisit for error
-  app.get("/v1/explorers/:id", (req, res) => {
-    console.log(`Api Explorers GET request ${new Date()}`);
-    console.log(`Getting explorer with id ${req.params.id}`);
-    const explorer = { id: 1, name: "Marco" };
-    res.status(200).json(explorer);
-  });
+// Create an endpoint that returns an explorer via the id
+// Revisit for error
+app.get("/v1/explorers/:id", (req, res) => {
+  console.log(`Api Explorers GET request ${new Date()}`);
+  console.log(`Getting explorer with id ${req.params.id}`);
+  const explorer = { id: 1, name: "Marco" };
+  res.status(200).json(explorer);
+});
+
+// Create an endpoint that creates an explorer
+app.post("/v1/explorers", (req, res) => {
+  console.log(`Api Explorers POST request ${new Date()}`);
+  const requestBody = req.body; // Client's params
+  console.log(requestBody);
+  res.status(201).json({ message: "Created" });
+});
+
+app.put("/v1/explorers/:id", (req, res) => {
+  console.log(`Api Explorers PUT request ${new Date()}`);
+  console.log(`Update explorer with id ${req.params.id}`);
+  const requestBody = req.body; // Parámetros de un cliente
+  res.status(200).json({ message: "Updated!" });
+});
+
+app.delete("/v1/explorers/:id", (req, res) => {
+  console.log(`APi Explorers DELETE request ${new Date()}`);
+  console.log(`Delete explorer with id ${req.params.id}`);
+  const requestBody = req.body; // Parámetros de un cliente
+  res.status(200).json({ message: "Deleted" });
 });
